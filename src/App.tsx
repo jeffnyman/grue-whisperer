5dde8d1f0b4f450daa8239f25682859d7e2fd070
Jeff Nyman <jeffnyman@gmail.com>
2026-03-19 12:09:14 -0500

    Initial project commit.

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Grue Whisperer</h1>
+      </header>
+
+      <main>
+        <p>Play LLM-Powered Text Adventures</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
