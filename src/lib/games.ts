0479db1da74f7f0ae676480b6bc1c2deb208ff9d
Jeff Nyman <jeffnyman@gmail.com>
2026-03-20 14:20:45 -0500

    chore: add zcode assets

--- src/lib/games.ts
+++ src/lib/games.ts
@@ -4,6 +4,7 @@ const STORAGE_KEYS = {
 
 export interface GameInfo {
   id: string;
+  file: string;
   title: string;
   subtitle?: string;
 }
@@ -11,16 +12,19 @@ export interface GameInfo {
 export const games: GameInfo[] = [
   {
     id: "zork1",
+    file: "zork1-r88-s840726.z3",
     title: "Zork I",
     subtitle: "The Great Underground Empire",
   },
   {
     id: "zork2",
+    file: "zork2-r48-s840904.z3",
     title: "Zork II",
     subtitle: "The Wizard of Frobozz",
   },
   {
     id: "zork3",
+    file: "zork3-r17-s840727.z3",
     title: "Zork III",
     subtitle: "The Dungeon Master",
   },
